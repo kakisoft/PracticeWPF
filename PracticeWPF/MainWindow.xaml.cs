460cb4cb52becf3a168f10e857afa9c69f1310bb
kakisoft <kakisttab@gmail.com>
2018-03-05 03:58:44 +0900

    edit main window

--- PracticeWPF/MainWindow.xaml.cs
+++ PracticeWPF/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace PracticeWPF
         {
             InitializeComponent();
         }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //var myWindow01 = new MyWindow01();
+                //myWindow01.Show(); //呼び出し元はアクティブ状態
+                ////myWindow01.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //var myWindow02 = new MyWindow02();
+                ////myWindow02.Show();
+                //myWindow02.ShowDialog(); //別ウィンドウが閉じるまで、呼び出し元は非アクティブ
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
     }
 }
