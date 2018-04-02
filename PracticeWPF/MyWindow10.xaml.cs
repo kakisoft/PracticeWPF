61c6f80fb5b39657e14eb0410ba3637abd6d07fc
kakisoft <kakisttab@gmail.com>
2018-04-02 22:31:08 +0900

    edit3

---
+++ PracticeWPF/MyWindow10.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PracticeWPF
+{
+    /// <summary>
+    /// MyWindow10.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MyWindow10 : Window
+    {
+        public MyWindow10()
+        {
+            InitializeComponent();
+
+
+            //==========< ボタン内で改行を入れる >==========
+            //(コード側で設定)
+            var b = new TextBlock();
+            b.Text = "TexoBlockのTextWrappingを使うと改行が楽だよ。";
+            b.TextWrapping = TextWrapping.Wrap;
+            Button02.Content = b;
+        }
+    }
+}
