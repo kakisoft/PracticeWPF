fae486c0b1ca418bef339689a23c3fc98fb9b0db
kakisoft <kakisttab@gmail.com>
2018-04-09 22:37:21 +0900

    edit

--- PracticeWPF/MyWindow16.xaml.cs
+++ PracticeWPF/MyWindow16.xaml.cs
@@ -2,20 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PracticeWPF
 {
