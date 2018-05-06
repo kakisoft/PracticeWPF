3e632af68c6f368473c4c92238dbc2c2c5f9266f
kakisoft <kakisttab@gmail.com>
2018-05-06 13:16:45 +0900

    edit

--- PracticeWPF/MyWindow36.xaml.cs
+++ PracticeWPF/MyWindow36.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,17 +45,108 @@ namespace PracticeWPF
         //パスを指定しない場合、bin/debug または bin/Release 
         //private const string ConnectionString = @"Data Source=Sample.db";
 
-        //パス指定
+        //-----< パス指定 >-----
         //string targetDirectory = System.Environment.CurrentDirectory + @"\..\..\Resources\";
         //string targetSource = "Sample01.db";
-        string ConnectionString = System.Environment.CurrentDirectory + @"\..\..\Resources\Sample01.db";
+        string ConnectionString = @"Data Source=" + System.Environment.CurrentDirectory + @"\..\..\Resources\Sample01.db";
 
 
+        #region 初期化
         public MyWindow36()
         {
             InitializeComponent();
+
+            InitializeThisWindowsParameters();
+            AddMyEvent();
+        }
+        private void InitializeThisWindowsParameters()
+        {
+
         }
 
+        private void AddMyEvent()
+        {
+            buttonCreate.Click += (sender, e) => buttonCreate_Click();
+            buttonInsert.Click += (sender, e) => buttonInsert_Click();
+            buttonSelect.Click += (sender, e) => buttonSelect_Click();
+            buttonSelect2.Click += (sender, e) => buttonSelect2_Click();
+        }
+        #endregion
+
+        #region テーブルの作成
+        private void buttonCreate_Click()
+        {
+            using (var con = new SQLiteConnection(ConnectionString))
+            {
+                con.Open();
+
+                using (var cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "CREATE TABLE T_MEMO(ID INTEGER PRIMARY KEY AUTOINCREMENT,MEMO TEXT) ";
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        #endregion
+
+        #region データの挿入
+        private void buttonInsert_Click()
+        {
+            using (var con = new SQLiteConnection(ConnectionString))
+            {
+                con.Open();
+
+                using (var cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO T_MEMO (MEMO) VALUES (@p_memo)";
+                    cmd.Parameters.Add(new SQLiteParameter("@p_memo", DateTime.Now.ToLongTimeString()));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        #endregion
+
+        #region テーブルの作成
+        private void buttonSelect_Click()
+        {
+            var list = new List<string>();
+
+            using (var con = new SQLiteConnection(ConnectionString))
+            {
+                con.Open();
+
+                using (var cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT MEMO FROM T_MEMO";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show(string.Join(",", list));
+        }
+        #endregion
+
+        #region 非接続型の読み出し
+        private void buttonSelect2_Click()
+        {
+            var adapter = new SQLiteDataAdapter("SELECT MEMO FROM T_MEMO", ConnectionString);
+
+            var ds = new DataSet();
+
+            adapter.Fill(ds, "T_MEMO");
+
+            MessageBox.Show(ds.Tables[0].Rows.Count.ToString());
+        }
+        #endregion
 
     }
 }
