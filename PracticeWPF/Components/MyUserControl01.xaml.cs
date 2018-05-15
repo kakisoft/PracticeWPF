26a97aaa0870e5a1fdc5ef17685486d4b67aebfd
kakisoft <kakisttab@gmail.com>
2018-05-15 23:56:34 +0900

    edit

---
+++ PracticeWPF/Components/MyUserControl01.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticeWPF.Components
+{
+    /// <summary>
+    /// MyUserControl01.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MyUserControl01 : UserControl
+    {
+        public MyUserControl01()
+        {
+            InitializeComponent();
+        }
+    }
+}
